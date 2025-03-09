ef829b84816a64af89e6b627cb42d369f0a92214
Eggi Aunur Rofiq <eggiar70@gmail.com>
2025-03-09 16:28:03 +0700

    Mulai Belajar JS

---
+++ app.js
@@ -0,0 +1,17 @@
+// if (1+1===2){
+//     console.log('Math still works Dude');
+// }
+
+// let angka = Math.floor(Math.random()*10);
+
+// if (angka > 5){
+//     console.log(`Angka lebih dari 5 angkanya adalah ${angka}`);
+// }
+
+// let password = prompt('Masukkan password: ');
+
+// if(password.length  >= 6 && password.indexOf(' ') === -1){
+//     console.log('Password valid');
+// } else {
+//     console.log('Password tidak valid');
+// }
